3c94a349c2da3971049eff9eec3beebb18c7a5c3
Jonas Majauskas <jonasmajauskas1@gmail.com>
2023-02-04 15:18:18 -0500

    added default view

--- controllers/places.js
+++ controllers/places.js
@@ -1,11 +1,30 @@
 const router = require('express').Router()
 
+// router.get('/', (req, res) => {
+//     res.render('GET /places')
+// })
+
 router.get('/', (req, res) => {
-    res.send('GET /places')
-})
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+    }]      
+    res.render('places/index', { places })
+    }
+)
+
+module.exports = router
 
 // router.get('*', (req, res) => {
 //     res.status(404).send('<h1>404 Page</h1>')
 // })
 
-module.exports = router
