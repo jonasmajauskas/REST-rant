f2ba24f193e38f61a5727f33613c53df961b7320
Jonas Majauskas <jonasmajauskas1@gmail.com>
2023-02-04 13:48:46 -0500

    part1 complete

--- controllers/places.js
+++ controllers/places.js
@@ -4,4 +4,8 @@ router.get('/', (req, res) => {
     res.send('GET /places')
 })
 
+// router.get('*', (req, res) => {
+//     res.status(404).send('<h1>404 Page</h1>')
+// })
+
 module.exports = router
--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-//Dependencies
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
-//endpoint
+app.use('/places', require('./controllers/places.js'))
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
 app.get('*', (req, res) => {
-    res.status('404').send('<h1>404 page</h1>') 
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
-//listen
 app.listen(process.env.PORT)
